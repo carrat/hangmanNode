4ebb8d30b066e26ed7966d3903185403ab6b2424
Andrew Carr <acarr@mail.utexas.edu>
2016-10-14 22:46:53 -0500

    emoved nnecessary index variable

--- game.js
+++ game.js
@@ -4,8 +4,6 @@ module.exports.selectWord = function(wordlist) {
 	mysteryWordSpaces = []; // array to hold the spaces representing correct letters
 	// Choose a mystery word for the active game
 	activeMysteryWord = wordlist[Math.floor(Math.random() * wordlist.length)];
-	// Find Index of Active Mystery Word
-	activeMysteryWordIndex = wordlist.indexOf(activeMysteryWord);
 	// add blank spaces to mysteryWordSpaces to generate gameboard
 	for (var i=0; i < activeMysteryWord.length; i++) {
 		if (activeMysteryWord[i] !== " ") {
